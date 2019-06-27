40e7af3c98c0c019e11749d967ee365eb2ff6437
Mike Diaz <miguelarturo@gmail.com>
2019-06-27 01:55:51 -0500

    finish

--- index.php
+++ index.php
@@ -13,55 +13,64 @@
         <script src="js/global.js"></script>
     </head>
     <body>
-        <div class="row" style ="margin-top: 20px;">
-            <div class="col s12 m8">
-                <h4>Users</h4>
-                <p>If you want to get contact information to specific user, just select group and them select him from the list below</p>
-                <div class="row">
-                    <div class="col s12">
-                        <div style="display: inline-block">Select group of users:</div>
-                        <div style="display: inline-block">
-                            <select onchange="athena.get_group(this);" class="browser-default">
-                                <option value="" disabled selected>Choose your option</option>
-                                <option value="users">Users</option>
-                                <option value="developers">Developers</option>
-                                <option value="designers">Designers</option>
-                                <option value="managers">Managers</option>
-                            </select>
+        <div class="main">
+            <div class="row">
+                <div class="col s12 m8">
+                    <h3>Users</h3>
+                    <p>If you want to get contact information to specific user, just select group and them select him from the list below</p>
+                    <div class="row">
+                        <div class="col s12">
+                            <div style="display: inline-block">Select group of users:</div>
+                            <div style="display: inline-block">
+                                <select id="data_group" onchange="athena.get_group(this);" class="browser-default">
+                                    <option value="" disabled selected>Choose your option</option>
+                                    <option value="users">Users</option>
+                                    <option value="developers">Developers</option>
+                                    <option value="designers">Designers</option>
+                                    <option value="managers">Managers</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="users" class="row"></div>
+                </div>
+                <div class="col s12 m4">
+                    <div class="row user_data">
+                        <div id="picture_background" class="col s12">
+                            &nbsp;
+                        </div>
+                        <div class="picture_content">
+                           <img id="user_picture" src="img/user.png" class="circle" /><br/><br/>
+                           <span id="user_name" class="white-text capitalize">&nbsp;</span><br/>
+                           <span id="user_group" class="white-text capitalize">&nbsp;</span>
+                        </div>
+                        <div id="information" style="display: none;">
+                            <div class="col s12">
+                                <span>Gender</span>
+                                <h4 id="user_gender" class="capitalize"></h4>
+                            </div>
+                            <div class="col s12">
+                                <span>Location</span>
+                                <h4 id="user_location" class="capitalize"></h4>
+                            </div>
+                            <div class="col s12">
+                                <span>Email Address</span>
+                                <h4 id="user_email"></h4>
+                            </div>
+                            <div class="col s12">
+                                <span>Phone Number</span>
+                                <h4 id="user_phone_number"></h4>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div id="users" class="row"></div>
             </div>
-            <div class="col s12 m4">
-                <div class="row" style="border-left: 1px solid #777;">
-                    <div class="col s12">
-                        <img id="user_picture" src="" class="circle img_contact_us" width="60px" height="60px" />
+            <div id="example" class="hide">
+                <div class="col s12 m6 user_badge">
+                    <div onclick="athena.show_user('@user_id@', this);" class="user_badge_content">
+                        <img src="@srcimg@" class="circle image_badge" />
+                        <span class="grey-text text-darken-2 capitalize">@name@</span>
                     </div>
-                    <div class="col s12">
-                        <p>Gender</p>
-                        <h4 id="user_gender"></h4>
-                    </div>
-                    <div class="col s12">
-                        <p>Location</p>
-                        <h4 id="user_location"></h4>
-                    </div>
-                    <div class="col s12">
-                        <p>Email Address</p>
-                        <h4 id="user_email"></h4>
-                    </div>
-                    <div class="col s12">
-                        <p>Phone Number</p>
-                        <h4 id="user_phone_number"></h4>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div id="example" class="hide">
-            <div class="col s12 m6">
-                <div onclick="athena.show_user('@user_id@');">
-                    <img src="@srcimg@" class="circle" style="vertical-align: middle; margin-left: 30px; margin-right: 10px;" width="60px" height="60px" />
-                    <span class="grey-text text-darken-2" style="font-size: 20px">@name@</span>
                 </div>
             </div>
         </div>
--- js/global.js
+++ js/global.js
@@ -17,18 +17,26 @@ var athena = {
                         .replace("@user_id@", result.email)
                         .replace("@name@", result.name.first + ' ' + result.name.last));
                 });
-              console.log(data);
             }
           });
     },
 
-    show_user: function(email) {
+    show_user: function(email, siht) {
         var user = athena.results.find(function(result) {
             return result.email === email;
         });
 
+        $('#users').find('.user_badge_content.active').each(function() {
+            $(this).removeClass('active');
+        });
+
         if (user) {
+            $(siht).addClass('active');
+            $("#information").show();
+            $("#picture_background").css("background-image", "url(" + user.picture.medium + ")");
             $("#user_picture").attr('src', user.picture.medium);
+            $("#user_name").html(user.name.first + ' ' + user.name.last);
+            $("#user_group").html($('#data_group').val());
             $("#user_gender").html(user.gender);
             $("#user_location").html(user.location.street + '<br />' + user.location.city +
                 '<br />' + user.location.state + '<br />' + user.location.sotcode);
